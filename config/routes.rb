11484ba9192c474cb40955c70e45060daab09161
Duzu <duzu.dev@gmail.com>
2024-03-23 12:32:07 +0100

    Added a homepage and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contact-us' => 'pages#contact-us'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
